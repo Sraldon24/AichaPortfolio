60af4771880fbe5dfb355e6ec0cd947d3eaea0ce
Amir Ghadimi <Sraldon24@gmail.com>
2026-02-18 10:06:11 -0500

    Force admin password update on deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn portfolio_core.wsgi:application --config gunicorn.conf.py
+web: python manage.py migrate && python manage.py ensure_admin && gunicorn portfolio_core.wsgi:application --config gunicorn.conf.py
--- portfolio/management/commands/ensure_admin.py
+++ portfolio/management/commands/ensure_admin.py
@@ -15,4 +15,7 @@ class Command(BaseCommand):
             User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Admin user "{username}" created'))
         else:
-            self.stdout.write(self.style.SUCCESS(f'Admin user "{username}" already exists'))
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Admin user "{username}" password updated'))
